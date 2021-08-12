3967b57f4039f684a3ad4678faba3ed2f7dcc64e
loc-dev <leonardoc.developer@gmail.com>
2021-08-12 09:32:48 -0400

    Descrevendo a função /register no Blueprint 'auth'

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -20,4 +20,50 @@ from flaskr.db import get_db
 # podemos usar subdomínio de URL associados ao Blueprint.
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+# Aqui será a nossa primeira visualização a função 'register', o usuário poderá registrar no aplicativo flaskr,
+# para isso a URL especificada será um conjunto da url_prefix: '/auth' + o decorador route()
+# da Blueprint '/register'.
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    # Se o usuário preencheu os dados ao formulário e confirmou, o método 'POST',
+    # encarregar de enviar os dados recebidos para o servidor.
+    if request.method == 'POST':
+        # Começamos a validar as seguintes entradas preenchidas pelo usuário,
+        # através do request.form, é um tipo de Dicionário [dict],
+        # podemos mapear os campos do formulário (chaves para dicionário)
+        username = request.form['username']
+        password = request.form['password']
+        # Chamando a função 'get_db', sendo aplicada na variável db
+        db = get_db()
+        # Na variável error, estamos declarando o valor None
+        error = None
 
+        # Condições para username e password
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        # Na validação não houve nenhum erro, portanto,
+        # os novos dados inseridos pelo usuário serão passados para o Banco de Dados.
+        # Em db.execute(), o método .execute(), está enviando a sintaxe SQL com espaços reservados da entrada do usuário,
+        # será escapado os valores para não ocorrer uma ataque de injeção de SQL.
+        # Com o generate_password_hash(), podemos usar em questão de segurança nas senhas declaradas passando em hash,
+        # nunca devemos armazenadas senhas diretramente no banco de dados.
+        # Toda modificação realizada ao Banco de dados  devemos usar o método .commit(), para salvar as alterações.
+        # A exceção .IntegrityError, ocorrerá caso o nome do usuário (username) já existir.
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f'User {username} is already registered.'
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
