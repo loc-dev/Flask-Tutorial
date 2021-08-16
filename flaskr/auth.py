35d1970e8a99b273febda48a588bda404c0ae64a
loc-dev <leonardoc.developer@gmail.com>
2021-08-16 07:18:01 -0400

    Ajustando alguns comentários e em progresso com a função /login no Blueprint 'auth'

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,8 +1,7 @@
 # 03 - Criando o Blueprint de Autenticação
 '''
-1º [Linha ?] - Importando o módulo functools - Fornece recursos como funções,
+1º [Linha 07] - Importando o módulo functools - Fornece recursos como funções,
 para trabalhar em outras funções e objetos que podem ser chamados
-
 '''
 
 import functools
@@ -26,7 +25,7 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     # Se o usuário preencheu os dados ao formulário e confirmou, o método 'POST',
-    # encarregar de enviar os dados recebidos para o servidor.
+    # encarrega-se de enviar os dados recebidos para o servidor.
     if request.method == 'POST':
         # Começamos a validar as seguintes entradas preenchidas pelo usuário,
         # através do request.form, é um tipo de Dicionário [dict],
@@ -46,10 +45,10 @@ def register():
 
         # Na validação não houve nenhum erro, portanto,
         # os novos dados inseridos pelo usuário serão passados para o Banco de Dados.
-        # Em db.execute(), o método .execute(), está enviando a sintaxe SQL com espaços reservados da entrada do usuário,
-        # será escapado os valores para não ocorrer uma ataque de injeção de SQL.
+        # Em db.execute(), o método .execute(), está enviando a sintaxe SQL com espaços reservados (?)
+        # de qualquer entrada do usuário, será escapado os valores para não ocorrer uma ataque de injeção de SQL.
         # Com o generate_password_hash(), podemos usar em questão de segurança nas senhas declaradas passando em hash,
-        # nunca devemos armazenadas senhas diretramente no banco de dados.
+        # nunca devemos armazená-las as senhas diretamente no banco de dados.
         # Toda modificação realizada ao Banco de dados  devemos usar o método .commit(), para salvar as alterações.
         # A exceção .IntegrityError, ocorrerá caso o nome do usuário (username) já existir.
         if error is None:
@@ -62,8 +61,54 @@ def register():
             except db.IntegrityError:
                 error = f'User {username} is already registered.'
             else:
+                # Após o armazenamento de dados do usuário, deve ser redirecionado para a página de login,
+                # com a função 'url_for' que gera uma URL dinamicamente com base nome do usuário.
+                # A função 'redirect', retornar uma resposta de redirecionamento para aquela função 'url_for'.
                 return redirect(url_for("auth.login"))
-
+        # Caso houver falhas, o método flask(), a mensagem que está sendo carregada são de erros,
+        # logo, podem ser recuperadas durante a renderização do Template.
         flash(error)
 
     return render_template('auth/register.html')
+
+# Nossa próxima visualização a função 'login', onde o usuário consegue acessar o aplicativo flaskr.
+# A URL especificada será um conjunto da url_prefix: '/auth' + o decorador route() da Blueprint '/login'.
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        # Iremos validar novamente as seguintes entradas preenchidas pelo usuário.
+        username = request.form['username']
+        password = request.form['password']
+        # Chamando a função 'get_db', sendo aplicada na variável db
+        db = get_db()
+        # Na variável error, estamos declarando o valor None
+        error = None
+        # Com o método .execute(), realizamos a consulta (query) através da sintaxe SQL.
+        # O método .fetchone(), retorna uma única sequência de consulta ou None, quando não há dados disponíveis.
+        # O método .fetchall(), será usado, pois, retorna uma lista de todas as linhas (restantes)
+        # de um resultado de consulta (query).
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?", (username,)
+        ).fetchone()
+
+        # Condições para username e password
+        if user is None:
+            error = 'Incorrect username.'
+        # Validando a senha preenchida atualmente em Hashes, com o hash armazenado no Banco de Dados.
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            # O objeto da classe Flask, conhecido como 'Session',
+            # é um dicionário [Dict] onde são armazenados os dados de solicitações.
+            # Caso todas as validações sejam bem-sucedida, o user com seu ['id'] é armazenado em uma nova session,
+            # essa session serão armazenados em um Cookie sendo enviado ao navegador,
+            # onde envia de volta com solicitações subsequentes.
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
